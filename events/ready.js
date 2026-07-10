5270fb38f3f5613a0f54a16ab21d817196c08703
kaan4yd3m1r-debug <kaan4yd3m1r@gmail.com>
2026-07-11 01:39:54 +0300

    Delete events directory

--- events/ready.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  name: 'ready',
-  once: true,
-  execute(client) {
-    console.log(`Bot ${client.user.tag} olarak giriş yaptı!`);
-    client.user.setActivity('c!komut yazarak beni sikin !');
-  }
-}
